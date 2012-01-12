3496f4851e9a9de88df50d7499a191afa6b95c38
Stijn Meurkens <meurkens@gmail.com>
2012-01-12 14:27:54 +0100

    Prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -14,7 +14,7 @@ git_dirty() {
   else
     if [[ $st == "nothing to commit (working directory clean)" ]]
     then
-      echo " %{$fg_bold[grey]%}$(git_prompt_info)%{$reset_color%}"
+      echo " %{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
     else
       echo " %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     fi
