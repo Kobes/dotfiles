c09608d071a1b9d2ffcb518caf142f12ab377963
Stijn Meurkens <meurkens@gmail.com>
2012-05-22 13:02:57 +0200

    OSX Defaults.

---
+++ osx/set-defaults.sh
@@ -0,0 +1,9 @@
+# Enable selection in quick look
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+# Disable the Ping sidebar in iTunes
+defaults write com.apple.iTunes disablePingSidebar -bool true
+
+# Disable all the other Ping stuff in iTunes
+defaults write com.apple.iTunes disablePing -bool true
+
