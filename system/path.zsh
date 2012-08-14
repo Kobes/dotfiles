1772aa51310e6b09d6a664fd06d199af2a928519
Stijn Meurkens <meurkens@gmail.com>
2012-08-14 13:54:02 +0200

    Path edits.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$PATH:$ZSH/bin:/usr/local/bin:/usr/local/sbin:.:bin:$HOME/.sfs"
+export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:.:bin:$HOME/.sfs:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
