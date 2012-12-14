9606468de6e621a973c9759d068b6a965061f2b2
Stijn Meurkens <meurkens@gmail.com>
2012-12-15 00:02:55 +0100

    Progress.

--- oracle/oracle.zsh
+++ oracle/oracle.zsh
@@ -4,4 +4,4 @@ export SQLPATH="/usr/local/oracle/instantclient_10_2"
 export TNS_ADMIN="/usr/local/oracle/instantclient_10_2"
 export PATH=$PATH:$DYLD_LIBRARY_PATH
 
-export NLS_LANG=American_America.US7ASCII
+export NLS_LANG=American_America.UTF8
