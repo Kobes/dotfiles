d559c66d03c028bf55fc6b70c13d5021490307a9
Stijn Meurkens <meurkens@gmail.com>
2013-01-16 22:07:12 +0100

    Git aliases.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,11 +17,10 @@
 # fi
 alias git=hub
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gf="git fetch"
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
