269f39323f8174b7b0ce07248b5cdbef452883ba
Stijn Meurkens <meurkens@gmail.com>
2013-02-07 13:59:34 +0100

    Adding --branches to git log.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,7 +17,7 @@
 # fi
 alias git=hub
 
-alias gl="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl="git log --branches --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gf="git fetch"
