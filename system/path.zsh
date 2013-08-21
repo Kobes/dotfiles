327c8f5f67c73acd7cef625c0e8cb1ecfacd67e4
Stijn Meurkens <meurkens@gmail.com>
2013-08-21 09:18:49 +0200

    pip-installed Python apps in /Users/meurkens/.dotfiles/bin:/usr/local/bin:/usr/local/sbin:/Users/meurkens/bin:.:bin:/Users/meurkens/.sfs:/Users/meurkens/.rbenv/shims:/Users/meurkens/.rbenv/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/opt/X11/bin:/usr/texbin:/usr/local/oracle/instantclient_10_2:/usr/local/share/npm/bin.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:.:bin:$HOME/.sfs:$PATH:/usr/local/share/npm/bin"
+export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:$HOME/bin:.:bin:$HOME/.sfs:$PATH:/usr/local/share/npm/bin:/usr/local/share/python"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
