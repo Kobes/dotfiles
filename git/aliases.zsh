c773301cfddc88b96a0972830f496c0f91365be7
Stijn Meurkens <meurkens@gmail.com>
2013-11-19 17:24:57 +0100

    Fixes git root.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -37,4 +37,4 @@ alias gb='git branch'
 compdef _git gb="git-branch"
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias gr='cd $(git root)'
+alias gr='cd $(git rev-parse --show-toplevel)'
